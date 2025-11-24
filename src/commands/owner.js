fc003659269743bd427b6678e9a3fd4ffdc6bf05
mingom626-hash <mingom626@gmail.com>
2025-11-24 22:38:54 +0100

    Create exclusive owner command system file

---
+++ src/commands/owner.js
@@ -0,0 +1,11 @@
+// Exclusive Owner Command System
+
+const exclusiveOwnerCommands = {
+    // your commands here
+};
+
+module.exports = exclusiveOwnerCommands;
+
+// Notes: This file is part of the exclusive owner command system. It contains commands that only the owner can execute.
+// Current Date and Time (UTC): 2025-11-24 21:38:45
+// Current User's Login: mingom626-hash
